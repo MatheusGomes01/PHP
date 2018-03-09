d04fd5ea8bd981febec9a6530b37d22b107c31d2
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-09 14:14:07 -0300

    switch().php

---
+++ switch().php
@@ -0,0 +1,20 @@
+<?
+	
+	switch (2) {
+		case 1:
+			echo "primerio";
+			break;
+
+		case 2:
+			echo "segundo";
+			break;
+
+		case 3:
+			echo "terceiro";
+			break;
+		
+		default:
+			echo "default";
+			break;
+	}
+?>
