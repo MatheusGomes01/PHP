011be82c620e2031aeb8c080a0a300acbdba2840
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-03-15 09:35:21 -0300

    usando as funções de string

---
+++ Funções PHP/strs.php
@@ -0,0 +1,13 @@
+<?
+	//strtolower
+	$texto = "STrToloWeR <br>";
+	echo strtolower($texto);
+
+	//strtoupper
+	$texto1 = "strToupper <br>";
+	echo strtolower($texto1);
+
+	//ucfirst
+	$texto2 = "ucfirst <br>";
+	echo ucfirst($texto2);
+?>
